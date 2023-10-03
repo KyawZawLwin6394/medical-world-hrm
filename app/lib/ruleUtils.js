27ef86a2450bcb266b7c19147ce25726255b6874
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-10-03 16:39:24 +0630

    here

--- app/lib/ruleUtils.js
+++ app/lib/ruleUtils.js
@@ -1,100 +1,75 @@
 'use strict';
 const moment = require('moment-timezone');
-const config = require('../../config/db')
+const config = require('../../config/db');
+const Setting = require('../models/setting');
 
-function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
+const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+const convertAndDisplayTZ = utcDate => moment.utc(utcDate).tz(config.timeZone).format('D-M-Y');
+const convertToWeekDayNames = utcDate => moment.utc(utcDate).tz(config.timeZone).format('ddd');
+const attendanceInputDate = utcDate => moment.utc(utcDate).tz(config.timeZone).format('YYYY-MM-DD');
 
-function convertAndDisplayTZ(utcDate) {
-    return moment.utc(utcDate).tz(config.timeZone).format('D-M-Y');
-}
+const calculatePenalty = (name, settings, salaryPerDay) => settings[name].pAmount > 0 ? settings[name].pAmount : (settings[name].pPercent * salaryPerDay) / 100;
 
-function convertToWeekDayNames(utcDate) {
-    return moment.utc(utcDate).tz(config.timeZone).format('ddd');
-}
-
-function attendanceInputDate(utcDate) {
-    return moment.utc(utcDate).tz(config.timeZone).format('YYYY-MM-DD');
-}
-
-function checkEmployeeAttendance(inputTimeStr, lateTimeStr, secLateTimeStr, halfDayTimeStr, salaryPerDay) {
+const checkEmployeeAttendance = (inputTimeStr, settings, salaryPerDay) => {
     const [inputHours, inputMinutes] = inputTimeStr.split(":").map(Number);
     let salary = salaryPerDay;
-
-    if (isLate(inputHours, inputMinutes, halfDayTimeStr)) {
-        salary -= salaryPerDay / 2;
-        console.log("The employee is late for Half Day.", inputTimeStr);
-    } else if (isLate(inputHours, inputMinutes, secLateTimeStr)) {
-        const penalty = (salaryPerDay * 1.7) / 100;
-        salary -= penalty;
-        console.log("The employee is late for Second Late.", inputTimeStr);
-    } else if (isLate(inputHours, inputMinutes, lateTimeStr)) {
-        const penalty = (salaryPerDay * 1) / 100;
-        salary -= penalty;
-        console.log("The employee is late for Late.", inputTimeStr);
-    } else {
-        console.log("The employee is on time or early.", inputTimeStr);
-    }
-
+    if (isLate(inputHours, inputMinutes, settings.fnlpenalty.pTime)) salary -= calculatePenalty('fnlpenalty', settings, salaryPerDay);
+    else if (isLate(inputHours, inputMinutes, settings.thdpenalty.pTime)) salary -= calculatePenalty('thdpenalty', settings, salaryPerDay);
+    else if (isLate(inputHours, inputMinutes, settings.secpenalty.pTime)) salary -= calculatePenalty('secpenalty', settings, salaryPerDay);
+    else if (isLate(inputHours, inputMinutes, settings.fstpenalty.pTime)) salary -= calculatePenalty('fstpenalty', settings, salaryPerDay);
+    else console.log("The employee is on time or early.", inputTimeStr);
     return salary;
-}
+};
 
-
-function isLate(inputHours, inputMinutes, thresholdTimeStr) {
+const isLate = (inputHours, inputMinutes, thresholdTimeStr) => {
     const [thresholdHours, thresholdMinutes] = thresholdTimeStr.split(":").map(Number);
     return inputHours > thresholdHours || (inputHours === thresholdHours && inputMinutes > thresholdMinutes);
-}
+};
 
-exports.calculatePayroll = (attendances, salaryPerDay, workingDays) => {
+exports.calculatePayroll = async (attendances, salaryPerDay, workingDays) => {
     try {
-        const paid = attendances.filter(item => item.isPaid === true) //including Attend and Dismiss
-        const entitledSalary = paid.reduce((accumulator, day) => {
-            const dayName = convertToWeekDayNames(day.date)
-            console.log(dayName)
-            if (day.clockIn && workingDays.includes(dayName)) {
-                const result = checkEmployeeAttendance(day.clockIn, "09:30", "10:00", "11:00", salaryPerDay)
-                //console.log(result, 'salary', dayName)
-                return accumulator + result
-            } else if (workingDays.includes(dayName) === false || day.attendType === 'Day Off') {
-                return accumulator + salaryPerDay
-            } else if (workingDays.includes(dayName) === true && day.clockIn === '') {
-                return accumulator
-            }
-
-        }, 0)
-        console.log(entitledSalary)
-        return ({ success: true, salary: entitledSalary })
+        const settings = await Setting.find({ _id: '651a47a7e259234bf081204c' });
+        if (!settings) return { error: true, message: 'Settings Not Found!' };
+        const entitledSalary = attendances
+            .filter(item => item.isPaid)
+            .reduce((acc, day) => {
+                const dayName = convertToWeekDayNames(day.date);
+                return acc + (
+                    (day.clockIn && workingDays.includes(dayName))
+                        ? checkEmployeeAttendance(day.clockIn, settings[0], salaryPerDay)
+                        : (!workingDays.includes(dayName) || day.attendType === 'Day Off')
+                            ? acc + salaryPerDay
+                            : (workingDays.includes(dayName) && !day.clockIn)
+                                ? acc
+                                : 0
+                );
+            }, 0);
+        return { success: true, salary: entitledSalary };
     } catch (error) {
-        return ({ success: false, message: error.message })
+        return { success: false, message: error.message };
     }
+};
 
-}
-exports.checkVarType = (varType1, varType2) => {
-    if (varType1.toLowerCase() !== varType2.toLowerCase()) return { success: false, message: 'Condition Types are not the same!' }
-    return { success: true }
-}
+
+
+exports.checkVarType = (varType1, varType2) => ({
+    success: varType1.toLowerCase() === varType2.toLowerCase(),
+    message: 'Condition Types are not the same!'
+});
 
 exports.prepareConditionValue = (condition1, condition2) => {
-    const conditions = ['date']
-    const filterCondition = conditions.filter(item => item === condition1.varType)
-    if (filterCondition.length !== 1) return ({ success: false, message: 'Condition Preparation Not Found!' })
+    const conditions = ['date'];
+    const filterCondition = conditions.filter(item => item === condition1.varType);
+    if (filterCondition.length !== 1) return { success: false, message: 'Condition Preparation Not Found!' };
     if (filterCondition[0] === 'date') {
-        const firstDate = new Date(condition1.value)
-        const secDate = new Date(condition2.value)
-        return (
-            {
-                success: true,
-                condition1Value: firstDate,
-                condition2Value: secDate
-            }
-        )
+        const firstDate = new Date(condition1.value);
+        const secDate = new Date(condition2.value);
+        return { success: true, condition1Value: firstDate, condition2Value: secDate };
     }
-}
+};
 
 exports.checkOperators = (param1, param2, operator) => {
     let success = false;
-
     switch (operator) {
         case 'gte':
             success = param1 >= param2;
@@ -112,6 +87,5 @@ exports.checkOperators = (param1, param2, operator) => {
             success = param1 === param2;
             break;
     }
-
     return { success };
 };
