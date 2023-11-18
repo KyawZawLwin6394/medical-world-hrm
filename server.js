a4cd7e20cbce618ff4af3a0df797ac0301aa6cc9
Oakar-Kyaw <oakarkyaw7090@gmail.com>
2023-11-18 18:49:34 +0630

    out of bound fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const express = require('express'),
   config = require('./config/db'),
   app = express(),
   server = require('http').Server(app),
-  port = 9000;
+  port = 5000;
 app.use(cors({ origin: '*' }));
 
 //mongoose.set('useCreateIndex', true) // to remove -> DeprecationWarning: collection.ensureIndex is deprecated. Use createIndex instead.
